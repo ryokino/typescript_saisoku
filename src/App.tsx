fa1716c469faec15e237b75956f4466b57de31d3
ryokino <ryo.kinoshita212@gmail.com>
2022-02-02 11:33:38 +0900

    TypeScriptの型

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'hello'; //リテラル型(今回のパターンでは 文字列リテラル と呼ぶ)
+
+let nameChange: string = 'Hello';
+let dummyNumber = 2;
+let bool = true;
+
+let array1 = [true, false, true];
+let array2 = [0, 1, 'hello'];
+
+// オブジェクトの型の名前
+interface NAME {
+	first: string;
+	last: string | null;
+}
+
+let nameObj: NAME = {
+	first: 'Yamada',
+	last: null,
+};
+
+const func1 = (x: number, y: number): number => {
+	return x + y;
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'></header>
+		</div>
+	);
 }
 
 export default App;
