152fbccd0171dc23576bb8872886488f75652ba1
ryokino <ryo.kinoshita212@gmail.com>
2022-02-02 16:39:48 +0900

    reacthooksの型

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
+
 interface Props {
 	text: string;
 }
 
+interface UserData {
+	id: number;
+	name: string;
+}
+
 const TestComponent: React.FC<Props> = (props) => {
+	const [count, setCount] = useState<number | null>(null);
+	const [user, setUser] = useState<UserData>({ id: 1, name: 'dummy' });
 	return (
 		<div>
 			<h1>{props.text}</h1>
+			<h1>{count}</h1>
 		</div>
 	);
 };
